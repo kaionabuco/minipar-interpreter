d441c0152d318b949da2d77b35db9f8799afbb6f
Kaio <74631438+kaionabuco@users.noreply.github.com>
2024-11-09 22:02:02 -0300

    Analisador lexico refeito

--- lexico.py
+++ lexico.py
@@ -1,86 +1,88 @@
-from enum import Enum, auto
-
-
-class TokenType(Enum):
-    INTEGER = auto()
-    STRING = auto()
-    BOOLEAN = auto()
-    KEYWORD = auto()
-    OPERADOR = auto()
-    SEPARADOR = auto()
-    IF = auto()
-    ELSE = auto()
-    WHILE = auto()
-    ATRIBUICAO = auto()
+# Tokens
+T_INT = 'INT'
+T_BOOL = 'BOOL'
+T_PLUS = 'PLUS'
+T_MINUS = 'MINUS'
+T_MULT = 'MULT'
+T_DIV = 'DIV'
+T_LPAREN = 'LPAREN'
+T_RPAREN = 'RPAREN'
 
 
 class Token:
-    def __init__(self, type, value):
-        self.type = type
+    def __init__(self, type_, value=None):
+        self.type = type_
         self.value = value
 
     def __repr__(self):
-        return f"Token({self.type}, {repr(self.value)})"
+        if self.value is not None:
+            return f'{self.type}:{self.value}'  # Imprime o tipo e o valor se há valor
+        else:
+            return f'{self.type}'  # Imprime apenas o tipo
 
 
 # Classe Lexer percorre string
-# TODO: get_boolean para 'true' e 'false'
-# TODO: separar cada operador em um tipo diferente
-# TODO: adicionar operadores de comparação lógica tipo "maior que" e "igual a" (requer tratamento)
 class Lexer:
     def __init__(self, text):
         self.text = text
-        self.pos = 0
-        self.current_char = self.text[self.pos]
+        self.pos = -1  # Posição do caractere no string, iniciamos em -1 pra avançar pra 0 no próximo método
+        self.current_char = None
+        self.proximo_caractere()
 
     # proximo_caractere avança cada caractere
     def proximo_caractere(self):
-        self.pos += 1
+        self.pos += 1  # Inicia na posição 0
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
-    # pular_espaco ignora espaço em branco " "
-    def pular_espaco(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.proximo_caractere()
-
-    # get_integer guarda número inteiro de n algarismos
-    def get_integer(self):
-        num_str = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            num_str += self.current_char
-            self.proximo_caractere()
-        return Token(TokenType.INTEGER, int(num_str))
-
-    # get_palavrareservada guarda as palavras "if, else, while"
-    def get_palavrareservada(self):
-        id_str = ''
-        while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
-            id_str += self.current_char
-            self.proximo_caractere()
-        # Checar qual palavra reservada
-        if id_str == 'if':
-            return Token(TokenType.IF, id_str)
-        elif id_str == 'else':
-            return Token(TokenType.ELSE, id_str)
-        elif id_str == 'while':
-            return Token(TokenType.WHILE, id_str)
-
-    def tokenize(self):
+    def criar_tokens(self):
         tokens = []
+
         while self.current_char is not None:
-            if self.current_char.isspace():
-                self.pular_espaco()
+            if self.current_char in ' \t':  # Pega espaços e tabs
+                self.proximo_caractere()
             elif self.current_char.isdigit():
                 tokens.append(self.get_integer())
             elif self.current_char.isalpha():
-                tokens.append(self.get_palavrareservada())
-            elif self.current_char in '+-*/=':
-                tokens.append(Token(TokenType.OPERADOR, self.current_char))
+                tokens.append(self.get_string())
+            elif self.current_char == '+':
+                tokens.append(Token(T_PLUS))
                 self.proximo_caractere()
-            elif self.current_char in '();':
-                tokens.append(Token(TokenType.SEPARADOR, self.current_char))
+            elif self.current_char == '-':
+                tokens.append(Token(T_MINUS))
                 self.proximo_caractere()
+            elif self.current_char == '*':
+                tokens.append(Token(T_MULT))
+                self.proximo_caractere()
+            elif self.current_char == '/':
+                tokens.append(Token(T_DIV))
+                self.proximo_caractere()
+            elif self.current_char == '(':
+                tokens.append(Token(T_LPAREN))
+                self.proximo_caractere()
+            elif self.current_char == ')':
+                tokens.append(Token(T_RPAREN))
+                self.proximo_caractere()
+            # elif self.current_char in ['True', 'False']:
+
+            #    tokens.append(Token(T_BOOL), bool(bool_str))
             else:
                 raise Exception(f"Caractere inválido: {self.current_char}")
 
         return tokens
+
+    def get_integer(self):  # Guarda número inteiro de n algarismos
+        num_str = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            num_str += self.current_char
+            self.proximo_caractere()
+        return Token(T_INT, int(num_str))
+
+    def get_string(self):  # Guarda booleano, string, ou bloqueia por palavra reservada
+        parsed_str = ''
+        while self.current_char is not None and self.current_char.isalpha():
+            parsed_str += self.current_char
+            self.proximo_caractere()
+        if parsed_str == 'True' or 'False':
+            return Token(T_BOOL, bool(parsed_str))
+        # elif palavra reservada
+        # elif string comum?
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Lorem ipsum
+import lexico
+
+
+def rodar(text):
+    lexer = lexico.Lexer(text)
+    tokens = lexer.criar_tokens()
+
+    return tokens
+
+
+while True:
+    texto = input('MiniPar > ')
+    result = rodar(texto)
+
+    print(result)
